5120333a5b74f509dfe284aac35ece337cbe68b5
Muhammad Masab <muhammadmasab00@gmail.com>
2026-07-22 21:06:45 +0500

    Login Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Login from './components/Auth/Login'
 
 const App = () => {
   return (
-    <div>
-      <h1>Muhammad Masab</h1>
-    </div>
+    <>
+    <Login/>
+    </>
   )
 }
 
