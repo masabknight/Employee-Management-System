61e6eeff4f4d0620061f9008ebf58507e3f4609b
Muhammad Masab <muhammadmasab00@gmail.com>
2026-07-22 21:59:00 +0500

    Task List

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -4,7 +4,7 @@ const TaskList = () => {
     return (
         <div id='tasklist' className='h-[55%] overflow-x-auto flex items-center justify-start gap-5 flex-nowrap w-full py-5 mt-10'>
 
-            <div className='flex-shrink-0 h-full w-[300px] p-5 bg-red-400 rounded-xl'>
+            <div className='shrink-0 h-full w-75 p-5 bg-red-400 rounded-xl'>
 
                 <div className='flex justify-between items-center'>
 
@@ -22,25 +22,7 @@ const TaskList = () => {
 
 
 
-            <div className='flex-shrink-0 h-full w-[300px] p-5 bg-green-400 rounded-xl'>
-
-                <div className='flex justify-between items-center'>
-
-                    <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
-                    
-                    <h4 className='text-sm'>20 feb 2024</h4>
-
-                </div>
-
-                <h2 className='mt-5 text-2xl font-semibold'>Make a youtube video</h2>
-
-                <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque magni mollitia rem maxime placeat porro.</p>
-
-            </div>
-
-
-
-            <div className='flex-shrink-0 h-full w-[300px] p-5 bg-blue-400 rounded-xl'>
+            <div className='shrink-0 h-full w-75 p-5 bg-green-400 rounded-xl'>
 
                 <div className='flex justify-between items-center'>
 
@@ -58,7 +40,25 @@ const TaskList = () => {
 
 
 
-            <div className='flex-shrink-0 h-full w-[300px] p-5 bg-yellow-400 rounded-xl'>
+            <div className='shrink-0 h-full w-75 p-5 bg-blue-400 rounded-xl'>
+
+                <div className='flex justify-between items-center'>
+
+                    <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
+                    
+                    <h4 className='text-sm'>20 feb 2024</h4>
+
+                </div>
+
+                <h2 className='mt-5 text-2xl font-semibold'>Make a youtube video</h2>
+
+                <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque magni mollitia rem maxime placeat porro.</p>
+
+            </div>
+
+
+
+            <div className='shrink-0 h-full w-75 p-5 bg-yellow-400 rounded-xl'>
 
                 <div className='flex justify-between items-center'>
 
